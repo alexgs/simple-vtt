f86c6fbfc2721804c9d01e7a3d95c97e56ff7761
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-03-06 16:41:50 -0500

    STT-2: Add basic NextAuth stuff

---
+++ webapp/pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,38 @@
+import NextAuth from 'next-auth';
+
+export default NextAuth({
+  providers: [],
+  // Database optional. MySQL, Maria DB, Postgres and MongoDB are supported.
+  // https://next-auth.js.org/configuration/databases
+  //
+  // Notes:
+  // * You must install an appropriate node_module for your database
+  // * The Email provider requires a database (OAuth providers do not)
+  // database: process.env.DATABASE_URL, // TODO
+
+  // The secret should be set to a reasonably long random string.
+  // It is used to sign cookies and to sign and encrypt JSON Web Tokens, unless
+  // a separate secret is defined explicitly for encrypting the JWT.
+  secret: process.env.SECRET,
+  session: {
+    strategy: 'jwt',
+
+    // Seconds - How long until an idle session expires and is no longer valid.
+    // maxAge: 30 * 24 * 60 * 60, // 30 days
+
+    // Seconds - Throttle how frequently to write to database to extend a session.
+    // Use it to limit write operations. Set to 0 to always update the database.
+    // Note: This option is ignored if using JSON Web Tokens
+    // updateAge: 24 * 60 * 60, // 24 hours
+  },
+  jwt: {
+    // A secret to use for key generation (you should set this explicitly)
+    secret: process.env.SECRET,
+  },
+  pages: {},
+  callbacks: {},
+  events: {},
+
+  // Enable debug messages in the console if you are having problems
+  debug: false,
+});

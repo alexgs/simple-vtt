696d5971e2ccebc5871cfbc9f80126d1322f215f
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-03-05 21:28:27 -0500

    STT-1: Add Emotion to webapp

--- webapp/pages/index.tsx
+++ webapp/pages/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Welcome = styled.div({
+  color: 'fuchsia',
+});
 
 const HomePage: React.FC = () => {
-  return <div>Welcome to Next.js!</div>;
+  return <Welcome>Welcome to Next.js!</Welcome>;
 };
 
 export default HomePage;
